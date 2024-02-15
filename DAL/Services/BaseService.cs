71e0b056b67ed140c67bfa9e46d48af2de1f833d
Elena Rottin <elena.rottin@gmail.com>
2024-02-15 22:44:26 +0100

    BaseService + ICRUDRepository

---
+++ DAL/Services/BaseService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace DAL.Services
+{
+    public abstract class BaseService
+    {
+        protected readonly string _connectionString;
+
+        protected BaseService(IConfiguration configuration, string dbname)
+        {
+            _connectionString = configuration.GetConnectionString(dbname) ?? throw new ArgumentException(nameof(dbname));
+        }
+    }
+}
---
+++ Shared/Repositories/ICRUDRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Shared.Repositories
+{
+    public interface ICRUDRepository<TEntity, TId> where TEntity : class
+    {
+        public IEnumerable<TEntity> Get();
+        public TEntity Get(TId id);
+        public TId Insert(TEntity data);
+        public void Update(TEntity data);
+        public void Delete(TId id);
+    }
+}
